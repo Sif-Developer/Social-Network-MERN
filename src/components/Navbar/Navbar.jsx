9207b1139268863d5a3f8e0cb9fbf623cfd7d87b
Sif Et tabaa <sifbusinesswork@gmail.com>
2022-12-15 18:09:01 +0100

    making css of the navbar, transitions of the text and icons working

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../../features/auth/authSlice";
+import { FileOutlined, HomeOutlined, MessageOutlined, UserOutlined } from "@ant-design/icons";
+import "./Navbar.scss";
 
 function Navbar() {
-
   const dispatch = useDispatch();
 
   const onLogout = () => {
@@ -12,14 +13,54 @@ function Navbar() {
   };
 
   return (
-    <span>
-      <Link to="/">Home </Link>
-      <Link to="/register"> Register </Link>
-      <Link to="/login"> Login </Link>
-      <Link to="/logout">Logout</Link>
-      <button onClick={onLogout}>Logout</button>
-    </span>
+    <nav>
+    <div className="navigation">
+      <ul>
+        <li className="list">
+          <Link className="link" to="/">
+          <span className="icon">
+            <HomeOutlined />
+          </span>
+            <p className="text">Home</p>
+          </Link>
+        </li >
+        <li className="list">
+          <Link className="link" to="/profile">
+          <span className="icon">
+            <UserOutlined />
+          </span>
+            <p className="text">Profile</p>
+          </Link>
+        </li >
+        <li className="list">
+          <Link className="link" to="/chat">
+          <span className="icon">
+            <MessageOutlined />
+          </span>
+          <p className="text">Chat</p>
+          </Link>
+        </li>
+        <li className="list">
+          <Link className="link" to="/posts">
+          <span className="icon">
+            <FileOutlined />
+          </span>
+            <p className="text">Posts</p>
+          </Link>
+        </li>
+      </ul>
+    </div>
+    </nav>
   );
 }
 
 export default Navbar;
+{
+  /* <Link  to="/">Home </Link>
+<Link to="/register"> Register </Link>
+<Link to="/login"> Login </Link>
+<Link to="/logout">Logout</Link> */
+}
+{
+  /* <button onClick={onLogout}>Logout</button> */
+}
