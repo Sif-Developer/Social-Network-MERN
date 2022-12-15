d29548297c7279d6f19bf22b44cea3bb0d4ede9c
Sif Et tabaa <sifbusinesswork@gmail.com>
2022-12-15 20:57:06 +0100

    navbar fixed, now can slide to right

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../../features/auth/authSlice";
@@ -12,43 +12,19 @@ import "./Navbar.scss";
 
 function Navbar() {
   const dispatch = useDispatch();
+  const [activeIndex, setActiveIndex] = useState(0);
 
   const onLogout = () => {
     dispatch(logout());
   };
-  
-  function activeLink() {
-    // Obtenemos todos los elementos con la clase "list"
-    const list = document.querySelectorAll(".list");
-    // Recorremos todos los elementos de la lista
-    list.forEach((item) => {
-      // Quitamos la clase "active" de todos los elementos
-      item.classList.remove("active");
-    });
-    // Agregamos la clase "active" al elemento que se hizo clic
-    this.classList.add("active");
-  }
-  // Agregamos un controlador de eventos "click" a cada elemento de la lista
-  // cuando el componente se monte en el DOM
-  useEffect(() => {
-    const list = document.querySelectorAll(".list");
-    list.forEach((item) => {
-      item.addEventListener("click", activeLink);
-    });
-    // Esta función se ejecutará cuando el componente se desmonte del DOM,
-    // y nos permite limpiar los controladores de eventos que agregamos
-    return () => {
-      list.forEach((item) => {
-        item.removeEventListener("click", activeLink);
-      });
-    };
-  }, []);
 
   return (
     <nav>
       <div className="navigation">
         <ul>
-          <li className="list active">
+          <li
+            className={`list ${activeIndex === 0 ? "activeLink" : ""}`}
+            onClick={() => setActiveIndex(0)}>
             <Link className="link" to="/">
               <span className="icon">
                 <HomeOutlined />
@@ -56,7 +32,9 @@ function Navbar() {
               <p className="text">Home</p>
             </Link>
           </li>
-          <li className="list">
+          <li
+            className={`list ${activeIndex === 1 ? "activeLink" : ""}`}
+            onClick={() => setActiveIndex(1)}>
             <Link className="link" to="/profile">
               <span className="icon">
                 <UserOutlined />
@@ -64,7 +42,9 @@ function Navbar() {
               <p className="text">Profile</p>
             </Link>
           </li>
-          <li className="list">
+          <li
+            className={`list ${activeIndex === 2 ? "activeLink" : ""}`}
+            onClick={() => setActiveIndex(2)}>
             <Link className="link" to="/chat">
               <span className="icon">
                 <MessageOutlined />
@@ -72,7 +52,9 @@ function Navbar() {
               <p className="text">Chat</p>
             </Link>
           </li>
-          <li className="list">
+          <li
+            className={`list ${activeIndex === 3 ? "activeLink" : ""}`}
+            onClick={() => setActiveIndex(3)}>
             <Link className="link" to="/posts">
               <span className="icon">
                 <FileOutlined />
@@ -80,10 +62,10 @@ function Navbar() {
               <p className="text">Posts</p>
             </Link>
           </li>
-          <div className="indicator"></div>
+          <div className="indicator">{activeIndex}</div>
         </ul>
       </div>
-      {activeLink()}
+      <></>
     </nav>
   );
 }
