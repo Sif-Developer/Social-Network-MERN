b22d9c1cde3b65b06bb083d204e0f534f850651a
Sif Et tabaa <sifbusinesswork@gmail.com>
2022-12-16 11:08:33 +0100

    created the space between the profile component and navbar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Register from "./components/Register/Register";
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile/>}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Profile.scss"
+
+function Profile() {
+  return (
+    <div className='profile-container'>
+        <div className='space-nav'></div>
+        <h1>Profile page</h1>
+    </div>
+  )
+}
+
+export default Profile
